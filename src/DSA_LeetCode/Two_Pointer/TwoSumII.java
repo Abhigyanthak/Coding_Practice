46c841ef8f7839efd3fc8ac2b7617c59a85f2d5c
abhigyan <abhigyanthakur2907@gmail.com>
2026-05-24 11:21:22 +0530

    Refactored code

--- src/DSA_LeetCode/Two_Pointer/TwoSumII.java
+++ src/DSA_LeetCode/Two_Pointer/TwoSumII.java
@@ -20,10 +20,9 @@ public class TwoSumII {
         return new int[2];
     }
     public static void main(String[] args) {
-        TwoSumII twoSumII = new TwoSumII();
         int[] nums = {2,7,11,15};
         int target = 9;
-        int[] result = twoSumII.twoSum(nums, target);
+        int[] result = TwoSumII.twoSum(nums, target);
         System.out.println(Arrays.toString(result));
     }
 }
